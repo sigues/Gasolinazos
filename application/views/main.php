515e9b7d1aac3a6f13fc9230ae45bff1a7f0914f
Salvador Villegas <salvador@sigues.org>
2013-05-31 17:56:37 -0500

    google analytics y modelo de bbdd

--- application/views/main.php
+++ application/views/main.php
@@ -34,6 +34,16 @@
 		<!--[if lte IE 8]><script src="<?=base_url()?>js/html5shiv.js"></script><![endif]-->
 	</head>
 	<body class="subpage">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41316581-1', 'startlogic.com');
+  ga('send', 'pageview');
+
+</script>
 <div id="fb-root"></div>
 <script src="<?=base_url()?>js/init.js"></script>
 <!--Below we include the Login Button social plugin. This button uses the JavaScript SDK to-->
